0ba9afa739bc21803bfabeeb8e25c8da4aa71461
Thomas-Andre <hetlesaetherdev@gmail.com>
2021-09-17 18:30:09 +0200

    first commit

---
+++ Sass webpack/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        filename: "bundled.js",
+        path: path.resolve(__dirname, "app")
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch("./app/**/*.html")
+        },
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000
+    },
+    mode: "development",
+ // watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", 'css-loader', {loader: "sass-loader"}]
+            }
+        ]
+    }
+}
---
+++ postCss webpack/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const postCSSPlugins = [
+    require("postcss-mixins"),
+    require("postcss-import")
+]
+
+module.exports = {
+    entry: "./app/assets/scripts/App.js",
+    output: {
+        filename: "bundled.js",
+        path: path.resolve(__dirname, "app")
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch("./app/**/*.html")
+        },
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000
+    },
+    mode: "development",
+ // watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", 'css-loader?url=true', {loader: "postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}}}]
+            }
+        ]
+    }
+}
